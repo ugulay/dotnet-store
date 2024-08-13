aec89500fda1c32f880b5a5c5142489f2d39606c
Uğur Gülay <ugurgulay@gmail.com>
2024-08-13 21:38:00 +0300

    Add eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,7 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended
+  ],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
